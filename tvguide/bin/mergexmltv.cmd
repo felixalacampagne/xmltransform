b9e6c3ba1ed4b7c2943ec546ce344e14fda613de
Chris <chris@felixalacampagne.be>
2023-03-25 22:33:54 +0100

    merge/combine test files

--- tvguide/bin/mergexmltv.cmd
+++ tvguide/bin/mergexmltv.cmd
@@ -1,1 +1,2 @@
-java -jar HTMLMaker.jar -merge "..\xml\mergexmltv.xml" "..\xml\tvguide.xmltv" "..\xml\xmltv_be_sky.xml"
+java -Xmx512M -jar HTMLMaker.jar -combine -alt "..\xml\xmltv_tvg_be.xml" -ref "..\xml\xmltv_epg.xml" -res "..\xml\xmltv_be.xml"
+java -Xmx512M -jar HTMLMaker.jar -merge "..\xml\mergexmltv.xmltv" "..\xml\xmltv_gb.xml" "..\xml\xmltv_be.xml"

1a4a8661f873f2434742039770c4947fcada49ad
Chris <chris@felixalacampagne.be>
2023-04-02 22:31:00 +0200

    Reorganise layout to conform to Maven requirement. Test files wont build-junit api doesn't exist even though dependency added as per Baeldung. Typical bloody Maven shirt. Build works when skipTests is enabled

--- src/test/java/com/scu/utils/NodeUtilsTest.java
+++ src/test/java/com/scu/utils/NodeUtilsTest.java
@@ -1,6 +1,8 @@
 package com.scu.utils;
 
-import static org.junit.Assert.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;

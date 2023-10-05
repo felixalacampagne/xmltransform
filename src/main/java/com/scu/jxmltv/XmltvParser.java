234bd8ea5d5663f43b70e7b684f9d2415ebc2a38
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2023-10-05 18:08:46 +0200

    convert xmltvdate to zonedate

--- src/main/java/com/scu/jxmltv/XmltvParser.java
+++ src/main/java/com/scu/jxmltv/XmltvParser.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import com.dontocsata.xmltv.XmlTVDataSorage;
 import com.dontocsata.xmltv.XmlTv;
@@ -13,6 +17,12 @@ import com.dontocsata.xmltv.XmlTvParseException;
 
 public class XmltvParser
 {
+   private static final DateTimeFormatter[] FORMATS = new DateTimeFormatter[] {
+         DateTimeFormatter.ofPattern("yyyyMMddHHmmss Z"),
+         DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.of("UTC")),
+         DateTimeFormatter.ofPattern("yyyyMMddHHmm"), DateTimeFormatter.ofPattern("yyyyMMddHH"),
+         DateTimeFormatter.ofPattern("yyyyMMdd") };
+
    static public void parse(String filename, XmlTVDataSorage storage) throws FileNotFoundException, XmlTvParseException
    {
       File xmlTvFile = new File(filename);
@@ -20,11 +30,23 @@ public class XmltvParser
    }
    static public void parse(File xmlTvFile, XmlTVDataSorage storage) throws FileNotFoundException, XmlTvParseException
    {
-      
+
       InputStream inStream = new FileInputStream(xmlTvFile);
       InputStream xmlTvStream = new BufferedInputStream(inStream);
-      
-      XmlTv xmltv = new XmlTv(xmlTvStream, storage);  
+
+      XmlTv xmltv = new XmlTv(xmlTvStream, storage);
       xmltv.parse();
+   }
+
+
+   public static ZonedDateTime XMLTVToZonedDateTime(String text) {
+      for (DateTimeFormatter dtf : FORMATS) {
+         try {
+            return ZonedDateTime.parse(text, dtf);
+         } catch (DateTimeParseException e) {
+            // no-op
+         }
+      }
+      throw new IllegalArgumentException("Unparseable: " + text);
    }
 }

8967fc0f81c666165561ccb238d2e1762a2eecb4
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2023-10-12 10:03:43 +0200

    Update XMLTVutils.java

--- src/main/java/com/scu/xmltv/XMLTVutils.java
+++ src/main/java/com/scu/xmltv/XMLTVutils.java
@@ -4,9 +4,28 @@ import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class XMLTVutils
 {
+private static Logger log = LoggerFactory.getLogger(XMLTVutils.class);
+
+// This is intended to match something like:
+// Short episode title: rest of description bla bla bla blab alblalbladelbllblblba
+// Of course, as soon as I implemented this there were a rash of descriptions like:
+// Pure description with no subtitle. Somewhere, over the rainbow; they decided to plant a : just for the hell of it.bla bla bla blab alblalbladelbl
+// which results in ridiculously long episode sub-titles. No simple way to avoid this.
+// It seems that \S matches all punctuation so try to ignore ":" which come after other punctuation.
+// Might need to limit the number of characters before the ':' since sub-titles are usually quite short
+// (yes there will be exceptions but long texts are not displayed well in the media player UI)
+// Could also only look for a subtitle if there is epnum info.
+private static final Pattern subtitPattern = Pattern.compile("^(?:\\.\\.\\.\\S.*?: )?([\\w ]*?): ");
+
    /**
     * SimpleDateFormat is too stupid to realise that not all of a date has been passed in,
     * ie 20060728 instead of 20060728000000 +0000.
@@ -63,30 +82,54 @@ public class XMLTVutils
       return dt;
    }
 
+
+//   private static final DateTimeFormatter[] FORMATS = new DateTimeFormatter[] {
+//         DateTimeFormatter.ofPattern("yyyyMMddHHmmss Z"),
+//         DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.of("UTC")),
+//         DateTimeFormatter.ofPattern("yyyyMMddHHmm"),
+//         DateTimeFormatter.ofPattern("yyyyMMddHH"),
+//         DateTimeFormatter.ofPattern("yyyyMMdd") };
+//   public static ZonedDateTime XMLTVToZonedDateTime(String text) {
+//      for (DateTimeFormatter dtf : FORMATS) {
+//         try {
+//            return ZonedDateTime.parse(text, dtf);
+//         } catch (DateTimeParseException e) {
+//            // no-op
+//         }
+//      }
+//      throw new IllegalArgumentException("Unparseable: " + text);
+//   }
+
+
 	public static ZonedDateTime getZDateFromXmltv(String xmltvdatetime)
 	{
 		ZonedDateTime zdt;
 		DateTimeFormatter format = DateTimeFormatter.ofPattern(getXMLTVDateFormat(xmltvdatetime));
+
+		// The default timezone for values without a timezone should be UTC. Need to figure out
+		// how to do that, eg. does setting the withZone to UTC override a timezone in the pattern
+		// DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.of("UTC")).
+		// See above XMLTVToZonedDateTime for an alternative solution....
 		zdt = ZonedDateTime.parse(xmltvdatetime, format);
 		return zdt;
 	}
-	
+
 	public static String getXmltvFromZDate(ZonedDateTime zdt)
 	{
 		DateTimeFormatter format = DateTimeFormatter.ofPattern(getXMLTVDateFormat(""));
 		String xmltv = format.format(zdt);
-		return xmltv;		
+		return xmltv;
 	}
 
 	public static ZonedDateTime getQuantizedDate(ZonedDateTime zdt, int quantum, int direction)
 	{
-		int mins = zdt.getMinute(); 
-		
-		
-		int off = mins % quantum;	
+		int mins = zdt.getMinute();
+
+
+		int off = mins % quantum;
 		if(off == 0)
 			return zdt;
-		
+
 		ZonedDateTime adj;
 		if(direction < 0)
 		{
@@ -96,7 +139,19 @@ public class XMLTVutils
 		{
 			adj = zdt.plusMinutes(quantum-off);
 		}
-		
+
 		return adj;
-	}	
+	}
+
+	public static Optional<String> getSubTitleFromDesc(String desc) {
+	   String subtitle = null;
+	   Matcher m = subtitPattern.matcher(desc);
+	   if(m.find())
+	   {
+	      subtitle = m.group(1);
+	      log.debug("getSubTitleFromDesc: found sub-title '{}' in '{}'", subtitle, desc);
+	   }
+	   return Optional.ofNullable(subtitle);
+	}
+
 }
